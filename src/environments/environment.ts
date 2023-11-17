ba6d20588ec0a3952e6dbcb88a723507e028d576
umang988 <umangtandel1860@gmail.com>
2023-11-17 17:39:31 +0530

    temp api url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : false,
-    // apiUrl : 'https://clothoton-api.onrender.com/api',
-    apiUrl : 'https://localhost:3000/api',
+    apiUrl : 'https://clothoton-api.onrender.com/api',
+    // apiUrl : 'http://localhost:3000/api',
 };
